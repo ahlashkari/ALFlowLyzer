656dea9c0a520979919be5ce8dd5fe10d0a0e246
moein-shafi <mosafer.moein@gmail.com>
2022-04-20 18:00:50 +0430

    Improve code style.

--- AppFlowMeter/features/feature.py
+++ AppFlowMeter/features/feature.py
@@ -3,6 +3,7 @@
 from abc import ABC, abstractmethod
 
 class Feature(ABC):
+    name: str
     @abstractmethod
-    def extract(self, flows: list) -> dict:
+    def extract(self, flow: object) -> dict:
         pass
--- AppFlowMeter/features/packets_numbers.py
+++ AppFlowMeter/features/packets_numbers.py
@@ -3,12 +3,6 @@
 from .feature import Feature
 
 class PacketsNumbers(Feature):
-    def extract(self, flows: list) -> dict:
-        data = { "src_ip": [], "dst_ip": [], "src_port": [], "dst_port": [], "packets_numbers": []}
-        for flow in flows:
-            data["src_ip"].append(flow.get_src_ip())
-            data["dst_ip"].append(flow.get_dst_ip())
-            data["src_port"].append(flow.get_src_port())
-            data["dst_port"].append(flow.get_dst_port())
-            data["packets_numbers"].append(len(flow.get_packets()))
-        return data
+    name = "packets_numbers"
+    def extract(self, flow: object) -> dict:
+        return len(flow.get_packets())
