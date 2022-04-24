2402284d34fdf2a421775024391299c05571bc16
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2022-04-24 10:33:38 +0530

    Update packets_len.py

--- AppFlowMeter/features/packets_len.py
+++ AppFlowMeter/features/packets_len.py
@@ -60,9 +60,87 @@ class PacketsLenSkewness(Feature):
         packets_len = [len(packet) for packet in flow.get_packets()]
         return statistics.median(packets_len)
 
-
 class PacketsLenCoefficientOfVariation(Feature):
     name = "packets_length_coefficient_of_variation"
     def extract(self, flow: object) -> float:
         packets_len = [len(packet) for packet in flow.get_packets()]
         return stats.skew(packets_len)
+    
+#Del L feature class
+class PacketsDelLenMin(Feature):
+    name = "packets_del_length_min"
+    def extract(self, flow: object) -> int:
+        packets_len = [len(packet) for packet in flow.get_packets()]
+        packets_del_len = [pkt - pkt_prev for pkt_prev, pkt in 
+                           zip(packets_len[:-1], packets_len[1:])]
+        print(packets_len)
+        print(packets_del_len)
+        return min(packets_del_len) if len(packets_del_len)>0 else 0
+    
+class PacketsDelLenMax(Feature):
+    name = "packets_del_length_max"
+    def extract(self, flow: object) -> int:
+        packets_len = [len(packet) for packet in flow.get_packets()]
+        packets_del_len = [pkt - pkt_prev for pkt_prev, pkt in 
+                           zip(packets_len[:-1], packets_len[1:])]
+        return max(packets_del_len) if len(packets_del_len)>0 else 0
+
+class PacketsDelLenMean(Feature):
+    name = "packets_del_length_mean"
+    def extract(self, flow: object) -> float:
+        packets_len = [len(packet) for packet in flow.get_packets()]
+        packets_del_len = [pkt - pkt_prev for pkt_prev, pkt in 
+                           zip(packets_len[:-1], packets_len[1:])]
+        return statistics.mean(packets_del_len) if len(packets_del_len)>0 else 0
+
+class PacketsDelLenMode(Feature):
+    name = "packets_del_length_mode"
+    def extract(self, flow: object) -> int:
+        packets_len = [len(packet) for packet in flow.get_packets()]
+        packets_del_len = [pkt - pkt_prev for pkt_prev, pkt in 
+                           zip(packets_len[:-1], packets_len[1:])]
+        return statistics.mode(packets_del_len) if len(packets_del_len)>0 else 0
+
+
+class PacketsDelLenVariance(Feature):
+    name = "packets_del_length_variance"
+    def extract(self, flow: object) -> float:
+        packets_len = [len(packet) for packet in flow.get_packets()]
+        packets_del_len = [pkt - pkt_prev for pkt_prev, pkt in 
+                           zip(packets_len[:-1], packets_len[1:])]
+        return statistics.pvariance(packets_del_len) if len(packets_del_len)>0 else 0
+
+
+class PacketsDelLenStandardDeviation(Feature):
+    name = "packets_del_length_standard_deviation"
+    def extract(self, flow: object) -> float:
+        packets_len = [len(packet) for packet in flow.get_packets()]
+        packets_del_len = [pkt - pkt_prev for pkt_prev, pkt in 
+                           zip(packets_len[:-1], packets_len[1:])]
+        return statistics.pstdev(packets_del_len) if len(packets_del_len)>0 else 0
+
+
+class PacketsDelLenMedian(Feature):
+    name = "packets_del_length_median"
+    def extract(self, flow: object) -> float:
+        packets_len = [len(packet) for packet in flow.get_packets()]
+        packets_del_len = [pkt - pkt_prev for pkt_prev, pkt in 
+                           zip(packets_len[:-1], packets_len[1:])]
+        return statistics.median(packets_del_len) if len(packets_del_len)>0 else 0
+
+
+class PacketsDelLenSkewness(Feature):
+    name = "packets_del_length_skewness"
+    def extract(self, flow: object) -> float:
+        packets_len = [len(packet) for packet in flow.get_packets()]
+        packets_del_len = [pkt - pkt_prev for pkt_prev, pkt in 
+                           zip(packets_len[:-1], packets_len[1:])]
+        return statistics.median(packets_del_len) if len(packets_del_len)>0 else 0
+
+class PacketsDelLenCoefficientOfVariation(Feature):
+    name = "packets_del_length_coefficient_of_variation"
+    def extract(self, flow: object) -> float:
+        packets_len = [len(packet) for packet in flow.get_packets()]
+        packets_del_len = [pkt - pkt_prev for pkt_prev, pkt in 
+                           zip(packets_len[:-1], packets_len[1:])]
+        return stats.skew(packets_del_len) if len(packets_del_len)>0 else 0
