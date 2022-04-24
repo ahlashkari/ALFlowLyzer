8e37d91954a7247f59250c4e2b770c42fa9fc82a
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2022-04-25 00:51:53 +0530

    Update __init__.py

--- AppFlowMeter/features/__init__.py
+++ AppFlowMeter/features/__init__.py
@@ -5,9 +5,9 @@ from .feature import Feature
 from .packets_len import PacketsLenMin, PacketsLenMax, PacketsLenMean, PacketsLenMedian, \
                          PacketsLenStandardDeviation, PacketsLenVariance, PacketsLenMode, \
                          PacketsLenSkewness, PacketsLenCoefficientOfVariation, \
-                         PacketsDelLenMin, PacketsDelLenMax, PacketsDelLenMean, PacketsDelLenMedian, \
-                         PacketsDelLenStandardDeviation, PacketsDelLenVariance, PacketsDelLenMode, \
-                         PacketsDelLenSkewness, PacketsDelLenCoefficientOfVariation    
+                         PacketsDeltaLenMin, PacketsDeltaLenMax, PacketsDeltaLenMean, PacketsDeltaLenMedian, \
+                         PacketsDeltaLenStandardDeviation, PacketsDeltaLenVariance, PacketsDeltaLenMode, \
+                         PacketsDeltaLenSkewness, PacketsDeltaLenCoefficientOfVariation    
 from .packets_numbers import PacketsNumbers, IncomingPacketsNumbers, OutgoingPacketsNumbers
 from .packets_time import ConnectionTime
 from .packets_rate import PacketsRate, IncomingPacketsRate, OutgoingPacketsRate
