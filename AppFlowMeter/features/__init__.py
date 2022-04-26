fc429ce34eca5140f03d7c2f611d6736316734d4
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2022-04-26 19:39:11 +0530

    Update __init__.py

--- AppFlowMeter/features/__init__.py
+++ AppFlowMeter/features/__init__.py
@@ -2,15 +2,12 @@
 
 from . import utils
 from .feature import Feature
-from .packets_len import PacketsLenMin, PacketsLenMax, PacketsLenMean, PacketsLenMedian, \
-                         PacketsLenStandardDeviation, PacketsLenVariance, PacketsLenMode, \
-                         PacketsLenSkewness, PacketsLenCoefficientOfVariation, \
-                         ReceivingPacketsDeltaLenMin, ReceivingPacketsDeltaLenMax, ReceivingPacketsDeltaLenMean, ReceivingPacketsDeltaLenMedian, \
+from .packets_len import ReceivingPacketsDeltaLenMin, ReceivingPacketsDeltaLenMax, ReceivingPacketsDeltaLenMean, ReceivingPacketsDeltaLenMedian, \
                          ReceivingPacketsDeltaLenStandardDeviation, ReceivingPacketsDeltaLenVariance, ReceivingPacketsDeltaLenMode, \
                          ReceivingPacketsDeltaLenSkewness, ReceivingPacketsDeltaLenCoefficientOfVariation, \
                          SendingPacketsDeltaLenMin, SendingPacketsDeltaLenMax, SendingPacketsDeltaLenMean, SendingPacketsDeltaLenMedian, \
                          SendingPacketsDeltaLenStandardDeviation, SendingPacketsDeltaLenVariance, SendingPacketsDeltaLenMode, \
-                         SendingPacketsDeltaLenSkewness, SendingPacketsDeltaLenCoefficientOfVariation
+                         SendingPacketsDeltaLenSkewness, SendingPacketsDeltaLenCoefficientOfVariation 
 from .packets_numbers import PacketsNumbers, IncomingPacketsNumbers, OutgoingPacketsNumbers
 from .packets_time import ConnectionTime
 from .packets_rate import PacketsRate, IncomingPacketsRate, OutgoingPacketsRate
