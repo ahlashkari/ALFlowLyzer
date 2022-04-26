0a4947738ae3a11a09b6d5cfd6fe7aff9c418950
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2022-04-26 19:38:34 +0530

    Update packets_len.py

--- AppFlowMeter/features/packets_len.py
+++ AppFlowMeter/features/packets_len.py
@@ -4,69 +4,6 @@ import statistics
 from scipy import stats
 from .feature import Feature
 from . import utils
-
-
-class PacketsLenMin(Feature):
-    name = "packets_length_min"
-    def extract(self, flow: object) -> int:
-        packets_len = [len(packet) for packet in flow.get_packets()]
-        return min(packets_len)
-
-
-class PacketsLenMax(Feature):
-    name = "packets_length_max"
-    def extract(self, flow: object) -> int:
-        packets_len = [len(packet) for packet in flow.get_packets()]
-        return max(packets_len)
-
-
-class PacketsLenMean(Feature):
-    name = "packets_length_mean"
-    def extract(self, flow: object) -> float:
-        packets_len = [len(packet) for packet in flow.get_packets()]
-        return statistics.mean(packets_len)
-
-
-class PacketsLenMode(Feature):
-    name = "packets_length_mode"
-    def extract(self, flow: object) -> int:
-        packets_len = [len(packet) for packet in flow.get_packets()]
-        return statistics.mode(packets_len)
-
-
-class PacketsLenVariance(Feature):
-    name = "packets_length_variance"
-    def extract(self, flow: object) -> float:
-        packets_len = [len(packet) for packet in flow.get_packets()]
-        return statistics.pvariance(packets_len)
-
-
-class PacketsLenStandardDeviation(Feature):
-    name = "packets_length_standard_deviation"
-    def extract(self, flow: object) -> float:
-        packets_len = [len(packet) for packet in flow.get_packets()]
-        return statistics.pstdev(packets_len)
-
-
-class PacketsLenMedian(Feature):
-    name = "packets_length_median"
-    def extract(self, flow: object) -> float:
-        packets_len = [len(packet) for packet in flow.get_packets()]
-        return statistics.median(packets_len)
-
-
-class PacketsLenSkewness(Feature):
-    name = "packets_length_skewness"
-    def extract(self, flow: object) -> float:
-        packets_len = [len(packet) for packet in flow.get_packets()]
-        return stats.skew(packets_len)
-
-
-class PacketsLenCoefficientOfVariation(Feature):
-    name = "packets_length_coefficient_of_variation"
-    def extract(self, flow: object) -> float:
-        packets_len = [len(packet) for packet in flow.get_packets()]
-        return stats.variation(packets_len)
     
 
 class PacketsDeltaLenBase(Feature):
