7da3a3ede685c923f4cb4c15ba3eb86375408c72
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2022-05-31 00:27:55 -0400

    Update utils.py

--- AppFlowMeter/features/utils.py
+++ AppFlowMeter/features/utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def calculate_duration(packets: list) -> float:
     packets_time = [packet.get_timestamp() for packet in packets]
     return max(packets_time) - min(packets_time)
@@ -27,3 +28,11 @@ def extract_successful_packets(packets: list, dst_ip: str) -> list:
         if packet.get_status_code() == 200:
             successful_packets.append(packet)
     return successful_packets
+
+
+def extract_request_response_packets(packets: list) -> list:
+    request_response_packets = []
+    for packet in packets:
+        if packet.get_req_status() or packet.get_response_status():
+            request_response_packets.append(packet)
+    return request_response_packets
