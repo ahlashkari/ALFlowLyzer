63981bd59ddae61e9ae3c97d0510a0430280d89d
moein-shafi <mosafer.moein@gmail.com>
2022-06-15 01:47:41 +0430

    Extract domain's name from DNS flows.

---
+++ AppFlowMeter/features/dns_related.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from .feature import Feature
+
+
+class DomainName(Feature):
+    name = "domain_name"
+    def extract(self, flow: object) -> str:
+        if flow.get_protocol() != "DNS":
+            return "not a dns flow"
+        return flow.get_domain_names()[0]
+
