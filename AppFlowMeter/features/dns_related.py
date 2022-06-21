f2aa29aea36dadfa4842925f5b9d5079705b8618
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2022-06-21 17:53:52 -0400

    Update dns_related.py

--- AppFlowMeter/features/dns_related.py
+++ AppFlowMeter/features/dns_related.py
@@ -14,17 +14,20 @@ class DomainName(Feature):
 class TopLevelDomain(Feature):
     name = "top_level_domain"
     def extract(self, flow: object) -> str:
+        tld_index = -2
         if flow.get_protocol() != "DNS":
             return "not a dns flow"
-        return "." + flow.get_domain_names()[0].split(".")[-2]
+        return "." + flow.get_domain_names()[0].split(".")[tld_index]
 
 
 class SecondLevelDomain(Feature):
     name = "second_level_domain"
     def extract(self, flow: object) -> str:
+        tld_index = -2
+        sld_index = -3
         if flow.get_protocol() != "DNS":
             return "not a dns flow"
-        return "." + flow.get_domain_names()[0].split(".")[-3] + "." + flow.get_domain_names()[0].split(".")[-2]
+        return "." + flow.get_domain_names()[0].split(".")[sld_index] + "." + flow.get_domain_names()[0].split(".")[tld_index]
 
 
 class DomainNameLen(Feature):
@@ -38,8 +41,9 @@ class DomainNameLen(Feature):
 class SubDomainNameLen(Feature):
     name = "subdomain_name_length"
     def extract(self, flow: object) -> str:
+        min_fqdn_len = 4
         if flow.get_protocol() != "DNS":
             return "not a dns flow"
-        elif len(flow.get_domain_names()[0].split(".")) < 4:
+        elif len(flow.get_domain_names()[0].split(".")) < min_fqdn_len:
             return None
         return len(flow.get_domain_names()[0].split(".")[0])
