7048693feced9ec4ac35381e053f6c797aec4846
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2022-08-14 15:04:01 +0530

    Update feature_extractor.py

--- AppFlowMeter/feature_extractor.py
+++ AppFlowMeter/feature_extractor.py
@@ -122,6 +122,7 @@ class FeatureExtractor(object):
                 ContinuousConsonantsMaxLen(),
                 ContinuousSameAlphabetMaxLen(),
                 VowelsConsonantRatio(),
+                ConvFreqVowelsConsonants(),
                 DistinctTTLValues(),
                 TTLValuesMin(),
                 TTLValuesMax(),
